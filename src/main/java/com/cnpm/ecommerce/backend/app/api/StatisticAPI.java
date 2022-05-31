c2598f56e4ff9eb063dee62f1dc23c0a16fde59d
pnhoa <hoapham1232000@gmail.com>
2022-05-31 22:29:49 +0700

    Add new statistic api for cart

--- src/main/java/com/cnpm/ecommerce/backend/app/api/StatisticAPI.java
+++ src/main/java/com/cnpm/ecommerce/backend/app/api/StatisticAPI.java
@@ -1,8 +1,12 @@
 package com.cnpm.ecommerce.backend.app.api;
 
 import com.cnpm.ecommerce.backend.app.dto.MessageResponse;
+import com.cnpm.ecommerce.backend.app.entity.Cart;
 import com.cnpm.ecommerce.backend.app.service.IStatisticService;
+import com.cnpm.ecommerce.backend.app.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -58,5 +62,46 @@ public class StatisticAPI {
         }
 
         return new ResponseEntity<>(new MessageResponse("Please provide time to get sold product", HttpStatus.BAD_REQUEST, LocalDateTime.now()), HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/order")
+    public ResponseEntity<?> getAllOrderBy(@RequestParam(name = "day", required = false) String day,
+                                           @RequestParam(name = "month", required = false) String month,
+                                           @RequestParam(name = "quarter", required = false) String quarter,
+                                           @RequestParam(name = "year", required = false) String year,
+                                           @RequestParam(defaultValue = "0") int page,
+                                           @RequestParam(defaultValue = "20") int limit,
+                                           @RequestParam(defaultValue = "id,ASC") String[] sort) throws ParseException {
+
+        try {
+            Pageable pagingSort = CommonUtils.sortItem(page, limit, sort);
+            Page<Cart> cartPage = null;
+
+            if(day != null) {
+                cartPage = statisticService.getAllCartByDay(day, pagingSort);
+
+                return new ResponseEntity<>(cartPage, HttpStatus.OK);
+            }
+            if(month != null) {
+                cartPage = statisticService.getAllCartByMonth(month, pagingSort);
+
+                return new ResponseEntity<>(cartPage, HttpStatus.OK);
+            }
+            if(quarter != null) {
+                cartPage = statisticService.getAllCartByQuarter(quarter, pagingSort);
+
+                return new ResponseEntity<>(cartPage, HttpStatus.OK);
+            }
+            if(year != null) {
+                cartPage = statisticService.getAllCartByYear(year, pagingSort);
+
+                return new ResponseEntity<>(cartPage, HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(new MessageResponse("Please provide time to get cart", HttpStatus.BAD_REQUEST, LocalDateTime.now()), HttpStatus.BAD_REQUEST);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(new MessageResponse("NOT_FOUND", HttpStatus.NOT_FOUND, LocalDateTime.now()), HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/cnpm/ecommerce/backend/app/service/StatisticService.java
+++ src/main/java/com/cnpm/ecommerce/backend/app/service/StatisticService.java
@@ -1,12 +1,17 @@
 package com.cnpm.ecommerce.backend.app.service;
 
+import com.cnpm.ecommerce.backend.app.entity.Cart;
+import com.cnpm.ecommerce.backend.app.entity.CartItem;
 import com.cnpm.ecommerce.backend.app.repository.CartItemRepository;
 import com.cnpm.ecommerce.backend.app.repository.CartRepository;
 import com.cnpm.ecommerce.backend.app.repository.ProductRepository;
 import com.cnpm.ecommerce.backend.app.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Base64Utils;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -149,6 +154,81 @@ public class StatisticService implements IStatisticService {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @Override
+    public Page<Cart> getAllCartByDay(String day, Pageable pagingSort) {
+
+        try {
+            Timestamp date = CommonUtils.convertStringToTimestamp(day, "dd-MM-yyyy");
+            Timestamp dateEndDate = CommonUtils.incrementTimestamp(date, 1);
+
+            Page<Cart> cartPage = cartRepository.findByCreatedDateBetween(date, dateEndDate, pagingSort);
+
+            return getCarts(cartPage);
+        } catch(Exception e)  {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    @Override
+    public Page<Cart> getAllCartByMonth(String month, Pageable pagingSort) {
+        try {
+            Timestamp date = CommonUtils.convertStringToMonth(month, "-");
+            Timestamp dateEndDate = CommonUtils.incrementTimestamp(date, CommonUtils.countDayOfMonth(month, "-"));
+
+            Page<Cart> cartPage = cartRepository.findByCreatedDateBetween(date, dateEndDate, pagingSort);
+
+            return getCarts(cartPage);
+        } catch(Exception e)  {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Page<Cart> getAllCartByQuarter(String quarter, Pageable pagingSort) {
+        try {
+            Timestamp date = CommonUtils.convertStringToQuarter(quarter, "-");
+            Timestamp dateEndDate = CommonUtils.incrementTimestamp(date, CommonUtils.countDayOfQuarter(quarter, "-"));
+
+            Page<Cart> cartPage = cartRepository.findByCreatedDateBetween(date, dateEndDate, pagingSort);
+
+            return getCarts(cartPage);
+        } catch(Exception e)  {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Page<Cart> getAllCartByYear(String year, Pageable pagingSort) {
+        try {
+            Timestamp date = CommonUtils.convertStringToYear(year);
+            Timestamp dateEndDate = CommonUtils.incrementTimestamp(date, 365);
+
+            Page<Cart> cartPage = cartRepository.findByCreatedDateBetween(date, dateEndDate, pagingSort);
+
+            return getCarts(cartPage);
+        } catch(Exception e)  {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Page<Cart> getCarts(Page<Cart> cartPage) {
+        for(Cart cart : cartPage.getContent()) {
+            cart.setCustomerIds(cart.getCustomer().getId());
+            for(CartItem cartItem : cart.getCartItems()) {
+                cartItem.setCartIds(cart.getId());
+                cartItem.setProductIds(cartItem.getProduct().getId());
+                cartItem.setProductName(cartItem.getProduct().getName());
+                cartItem.setProductThumbnail(Base64Utils.encodeToString(cartItem.getProduct().getThumbnailArr()));
+            }
+        }
+        return  cartPage;
     }
 
 
